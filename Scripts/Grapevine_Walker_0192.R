77e810e1ccb7b24927c74f6d73f6bbdeec9fce3c
Steve Henderson <hendo999@hotmail.com>
2019-10-30 00:14:32 +1030

    Packages & libraries

---
+++ Scripts/Grapevine_Walker_0192.R
@@ -0,0 +1,12 @@
+#install and load packages
+install.packages("janitor")
+install.packages("dplyr")
+install.packages("rowr")
+
+library(tidyverse) #for tidy stuff
+library(ggplot2)
+library(readxl) #for reading Excel files
+library(plyr) #for join_all to join multiple dataframes, note stuffs up rename function in dplyr
+library(dplyr)
+library(janitor) #for tidying up column names
+library(rowr) #for cbind.all function
